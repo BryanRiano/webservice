af66aae7a7f807fd709d70ab25c23b74c44d5eec
brayan <brayanriano21@gmail.com>
2020-02-20 18:25:42 -0500

    se crea servidor web

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
+var config = require('./config/config');
+var globals = config['database'];
+const cors = require('cors')
+
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+
+app.use(bodyParser.json());
+
+//routes
+require('./routes/vehiculo')(app);
+
+app.listen(3000, () => {
+    console.log('Server started! on port 3000');
+});
